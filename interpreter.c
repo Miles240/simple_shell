3d340a1090bb62dfcc59d1b7be31b04d8d78b7d7
Miles2400 <134848387+Miles240@users.noreply.github.com>
2023-11-15 16:23:02 +0100

    ..

--- interpreter.c
+++ interpreter.c
@@ -1,15 +1,14 @@
+/**
+ *interpreter - function
+ *@val: var
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "main.h"
 #include <sys/types.h>
 #include <sys/wait.h>
-
-/**
- *interpreter - function
- *@val: var
- *
- */
 
 void interpreter(const char *val)
 {
@@ -30,4 +29,23 @@ void interpreter(const char *val)
 	{
 		fprintf(stderr, "%s: No such file or directory\n", val);
 	}
+}
+int main()
+{
+	char command[100];
+
+	while (1)
+	{
+		printf("Enter a command: ");
+		if (scanf("%s", command) == 1)
+		{
+			interpreter(command);
+		}
+		else
+		{
+			printf("Invalid command input.\n");
+		}
+	}
+
+	return 0;
 }
