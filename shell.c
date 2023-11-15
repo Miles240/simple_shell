3ca0ab9bcfcc8c827242ae5b7ea4e9c39d29b550
ayuure <bertinaayuure0147@gmail.com>
2023-11-15 18:11:43 +0000

    works like what they asked

--- shell.c
+++ shell.c
@@ -1,10 +1,6 @@
-/*
- * simple_shell - function
-*/
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -17,13 +13,8 @@ void simple_shell(void)
 {
     char *buffer = NULL;
     size_t bufsize = 0;
-    char *program_name = "./shell";
-	pid_t child_pid;
-char *token;
-                char *args[100];
+    char *program_name = "./shell"; // Change this to your program name
 
-                int i = 0;
-                
     while (1)
     {
         write(STDOUT_FILENO, "#cisfun$ ", 9);
@@ -38,39 +29,23 @@ char *token;
             perror("getline");
             exit(EXIT_FAILURE);
         }
-
         if (buffer[0] != '\n')
         {
             buffer[strlen(buffer) - 1] = '\0';
-
-            
-            child_pid = fork();
-
+            pid_t child_pid = fork();
             if (child_pid == -1)
             {
                 perror("fork");
                 exit(EXIT_FAILURE);
             }
-
             if (child_pid == 0)
             {
-
-                token = strtok(buffer, " ");
-
-                while (token != NULL)
+                if (execve(buffer, (char *[]){buffer, NULL}, NULL) == -1)
                 {
-                    args[i++] = token;
-                    token = strtok(NULL, " ");
+                    perror(program_name);
+                    _exit(EXIT_FAILURE);
                 }
-
-                args[i] = NULL;
-
-                if (execvp(args[0], args) == -1)
-                {
-	perror(program_name);
-                    exit(EXIT_FAILURE);
-                }		
-                }
+            }
             else
             {
                 waitpid(child_pid, NULL, 0);
