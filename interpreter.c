368b4d0b24560c6c5f74a73440ac44f5e0ea852e
Miles2400 <134848387+Miles240@users.noreply.github.com>
2023-11-15 16:07:38 +0100

    updated betty issues

--- interpreter.c
+++ interpreter.c
@@ -1,26 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+
 /**
  *interpreter - function
  *@val: var
  *
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "main.h"
-#include<sys/types.h>
-#include <sys/wait.h>
 
-void interpreter(const char *val) {
-    if (access(val, X_OK) == 0) {
-        if (fork() == 0) {
-            execl(val, val, (char *)NULL);
-            perror(val);
-            exit(1);
-        } else {
-            wait(NULL);
-        }
-    } else {
-        fprintf(stderr, "%s: No such file or directory\n", val);
-    }
-
+void interpreter(const char *val)
+{
+	if (access(val, X_OK) == 0)
+	{
+		if (fork() == 0)
+		{
+			execl(val, val, (char *)NULL);
+			perror(val);
+			exit(1);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "%s: No such file or directory\n", val);
+	}
 }
